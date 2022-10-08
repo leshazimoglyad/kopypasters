e19ae9b1b3522789024900256f38ab5e0586789b
DanStelmakh <danylo.stelmakh@gmail.com>
2022-10-08 23:22:53 +0300

    upgrade mixins, add modal window with some js

--- src/js/modal/modal-film.js
+++ src/js/modal/modal-film.js
@@ -3,10 +3,10 @@ const refs = {
         closeBtn: document.querySelector(".close_btn"),
         openBtn: document.querySelector(".movies-section__grid"),
 };
-refs.openBtn.addEventListener("click", openModal);
+refs.openBtn.addEventListener("click", openModalWindow);
 refs.closeBtn.addEventListener("click", closeModal);
 
-function openModal(e) {
+function openModalWindow(e) {
         const filmCard = e.target.nodeName;
         if (filmCard !== "IMG") return;
         refs.modalWindow.classList.toggle("is-hidden");
