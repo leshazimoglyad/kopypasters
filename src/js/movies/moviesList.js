59f5bbac9b906faa2175fae7fe2d426ce5960ffd
Igor Smorodsky <ingerard@gmail.com>
2022-10-09 16:58:20 +0300

    reorganized

--- src/js/movies/moviesList.js
+++ src/js/movies/moviesList.js
@@ -1,5 +1,6 @@
-import { loadFromStorage } from "./services/storage.js";
-import { createMovieCard } from "./style/movieCard";
+import { getRndInteger } from "../helpers/index.js";
+import { loadFromStorage } from "../services/storage.js";
+import { createMovieCard } from "./movieCard";
 
 // Clear gallery
 function clearGallery() {
@@ -27,53 +28,33 @@ function renderMoviesList(dataJSON) {
 
         // Append new photos to DOM
         gallery.insertAdjacentHTML("beforeend", moviesCards);
-
-        // ЩЕ ЗГОДИТЬСЯ
-        // Get all new cards
-        // const newCards = gallery.querySelectorAll(".movies-section__grid__card.new");
+        
+        // Get all cards
+        const cards = gallery.querySelectorAll(".movies-section__card");
 
         // Add events to cards
-        // attachEventsToCards(newCards);
-
-        // return newCards;
+        attachOnloadToCards(cards);
 }
 
-// ЩЕ ЗГОДИТЬСЯ
-// Events on click buttons in cards
 // Add events on pictures and checking downloading complete
-function attachEventsToCards(newCards) {
-        newCards.forEach((card) => {
-                // Get icons refs
-                const icons = card.querySelector(".movies-section__grid__icon-wrapper");
-
+function attachOnloadToCards(cards) {
+        cards.forEach((card) => {
                 // Get link and img refs
-                const link = card.firstElementChild;
-                const img = link.firstElementChild;
+                const img = card.firstElementChild;
 
+                // console.log(img);
                 // If pic is loaded - remove skelet effect and add loaded mark on img
                 img.onload = () => {
                         // Added random showing delay
                         setTimeout(() => {
-                                card.classList.remove("new");
                                 img.classList.add("loaded");
                         }, getRndInteger(400, 1000));
                 };
         });
 }
 
-// Random nums
-function getRndInteger(min, max) {
-        return Math.floor(Math.random() * (max - min)) + min;
-}
-
 // Initialization render gallery
 export function initRender(dataJSON) {
-
-        // Remove blank pic
-        // const gallery = document.querySelector(".intro");
-        // gallery.style.setProperty("display", "none");
-        // gallery.classList.remove("gallery--empty-page");
-
         // Clear all gallery
         clearGallery();
 
