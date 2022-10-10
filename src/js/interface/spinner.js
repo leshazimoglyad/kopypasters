69be6bde9e78669b6b46aa4af7f63968409ccf17
Igor Smorodsky <ingerard@gmail.com>
2022-10-10 10:51:35 +0300

    created spinner to another component

--- src/js/interface/spinner.js
+++ src/js/interface/spinner.js
@@ -1,5 +1,5 @@
 // Toggle spinner
 export function loadingSpinnerToggle() {
-    const spinnerElem = document.querySelector(".movies-section__loading-spinner");
+    const spinnerElem = document.querySelector(".spinner");
     spinnerElem.classList.toggle("visually-hidden");
 }
